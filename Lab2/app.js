40adda5e7d6e4cac47ae33d417bfb4ace2578acf
Maximilian Bleier <maximilian.bleier75@gmail.com>
2024-11-28 13:17:55 +0100

    test

--- Lab2/app.js
+++ Lab2/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Azure!');
+  res.send('Hello, Azure! Get');
 });
 
+app.post('/', (req, res) => {
+    res.send('Hello, Azure! Post');
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App running on port ${port}`));
 
