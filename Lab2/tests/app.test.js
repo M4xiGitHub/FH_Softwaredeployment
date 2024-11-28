003542c3eda72dd71eaa16661ae80c9f080931cd
Maximilian Bleier <maximilian.bleier75@gmail.com>
2024-11-28 14:06:37 +0100

    test

--- Lab2/tests/app.test.js
+++ Lab2/tests/app.test.js
@@ -3,8 +3,8 @@ const app = require('../app');
 
 describe('GET /', () => {
   it('should return Hello, Azure!', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get('api/message');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello, Azure!');
+    expect(res.text).toBe('Hello from the API');
   });
 });
